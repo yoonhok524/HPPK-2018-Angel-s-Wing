c2fb632f049d317cfd780f241515cd6689be8354
AUTH\kimyoonh <youknow.yoonho@gmail.com>
2018-10-02 16:05:22 +0900

    Create android project

---
+++ app/src/main/java/com/youknow/hppk_2018_angel_s_wing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.youknow.hppk_2018_angel_s_wing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
